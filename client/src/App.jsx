a4512ccce8246b03251eb6ac09415989466759e4
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-10 03:24:23 +0000

    modified pacage.json, App.jsx and created a directory pages with the folowing files About.jsx, Home.jsx, Profile.jsx SignIn.jsx and SignUp.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn"; // Correctly imported SignIn component
+import SignUp from "./pages/SignUp"; // Correctly imported SignUp component
 
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/sign-in"
+          element={<SignIn />} // Correctly used SignIn component
+        />
+        <Route
+          path="/sign-up"
+          element={<SignUp />} // Correctly used SignUp component
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/profile"
+          element={<Profile />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
