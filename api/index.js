cc655204e192f60e78e39497de1b7651476949bd
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-12 04:28:00 +0000

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRouter = require("./routes/user.route.js");
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,3 +17,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
