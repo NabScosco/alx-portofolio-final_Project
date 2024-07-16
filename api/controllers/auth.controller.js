817924f0429f768665dc7ab939cf6b35943259e2
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-16 21:45:02 +0000

    completed signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -7,8 +8,8 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.staus(201).json("User created successfully");
+    res.status(201).json("User created successfully");
   } catch (error) {
-    next(error);
+    next(errorHandler(500, error.message));
   }
 };
