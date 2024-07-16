928a7e8a236e9ce10ccc40335968d6447689eea1
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-16 12:17:07 +0000

    create signup api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,3 @@
-const test = (req, res) => {
+export const test = (req, res) => {
   res.json({ message: "Api route is working" });
 };
-
-module.exports = { test };
--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const userRouter = require("./routes/user.route.js");
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
