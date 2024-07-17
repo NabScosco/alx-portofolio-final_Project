8fcb6e53b1f129d0c52f53be10f7998a64acf91a
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-17 06:25:02 +0000

    added redux toolkit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -20,18 +20,18 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-  const stausCode = err.stausCode || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(stausCode).json({
+  return res.status(statusCode).json({
     success: false,
-    stausCode,
+    statusCode,
     message,
   });
 });
