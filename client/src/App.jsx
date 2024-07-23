a3ec307a64c6684fb792d80081ed0dbeada94d6b
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-07-23 05:21:19 +0000

    Created listing page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn"; // Correctly imported SignIn component
 import SignUp from "./pages/SignUp"; // Correctly imported SignUp component
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 export default function App() {
   return (
@@ -32,6 +33,10 @@ export default function App() {
           <Route
             path="/profile"
             element={<Profile />}
+          />
+          <Route
+            path="/create-listing"
+            element={<CreateListing />}
           />
         </Route>
       </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ import {
   signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -179,6 +180,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update profile"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listings"}
+        >
+          Crearte Listings
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
